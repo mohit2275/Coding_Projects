4577901a8b8ebe2797c39a8a52985300460b937c
mohit2275 <mohit.chauhan2275@gmail.com>
2023-05-11 10:49:41 +0530

    fourth commit

---
+++ 5. Vertical Tabs/script.js
@@ -0,0 +1,8 @@
+function drop(x, y, z, a, b, c) {
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+    a.style.backgroundColor = "rgb(172, 156, 156)";
+    b.style.backgroundColor = "rgb(216, 213, 213)";
+    c.style.backgroundColor = "rgb(216, 213, 213)";
+}

97a5a8f908b3e5e6247d5979f53f0882807635d1
mohit2275 <mohit.chauhan2275@gmail.com>
2023-05-20 15:08:02 +0530

    Three types of side navigation

---
+++ 15. Side Navigation/Side nav Overlay/script.js
@@ -0,0 +1,11 @@
+let showNav = () => {
+    document.getElementById('content').style.left = "0%";
+    document.getElementById('content').style.visibility = "visible";
+}
+
+let hideNav = () => {
+    document.getElementById('content').style.left = "-40%";
+    document.getElementById('content').style.visibility = "hidden";
+}
+
+
---
+++ 15. Side Navigation/Side nav with full width/script.js
@@ -0,0 +1,8 @@
+let showNav = () => {
+    document.getElementById('content').style.width = "100%";
+}
+
+let hideNav = () => {
+    document.getElementById('content').style.width = "0px";
+}
+

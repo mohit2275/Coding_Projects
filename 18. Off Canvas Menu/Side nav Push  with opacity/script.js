43c866ca305b359476a8f37d3ea8919b67d0730a
mohit2275 <mohit.chauhan2275@gmail.com>
2023-05-24 23:47:48 +0530

    Off Canvas

---
+++ 18. Off Canvas Menu/Side nav Push  with opacity/script.js
@@ -0,0 +1,13 @@
+let showNav = () => {
+    document.getElementById('content').style.width = "fit-content";
+    document.getElementById('main').style.marginLeft = "24%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+let hideNav = () => {
+    document.getElementById('content').style.width = "0px";
+    document.getElementById('main').style.marginLeft = "0%";
+    document.body.style.backgroundColor = "white";
+}
+
+
---
+++ 18. Off Canvas Menu/Side nav Push/script.js
@@ -0,0 +1,11 @@
+let showNav = () => {
+    document.getElementById('content').style.width = "fit-content";
+    document.getElementById('main').style.marginLeft = "15%";
+}
+
+let hideNav = () => {
+    document.getElementById('content').style.width = "0px";
+    document.getElementById('main').style.marginLeft = "0%";
+}
+
+

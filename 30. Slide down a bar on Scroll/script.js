0ca014b3ca1b31a030db89ee47d72a470eb2cf2a
mohit2275 <mohit.chauhan2275@gmail.com>
2023-06-12 22:12:29 +0530

    Slide down a bar on scroll

---
+++ 30. Slide down a bar on Scroll/script.js
@@ -0,0 +1,14 @@
+let navbar = document.getElementById('navbar')
+
+window.onscroll = function () {
+    mohit();
+}
+
+function mohit() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        navbar.style.top = "0"
+    }
+    else {
+        navbar.style.top = "-55px"
+    }
+}

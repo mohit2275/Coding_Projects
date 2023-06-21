19cc55e5471b7051131118e140964b63025a4745
mohit2275 <mohit.chauhan2275@gmail.com>
2023-06-21 23:24:23 +0530

    Mobile Menu

---
+++ 44. Mobile Menu/Vertical Mobile Navbar/script.js
@@ -0,0 +1,8 @@
+let btn = document.getElementById('btn');
+let navbar = document.getElementById('navbar');
+
+
+btn.addEventListener("click", function () {
+    console.log('Hello');
+    navbar.classList.toggle('mohit');
+}) 

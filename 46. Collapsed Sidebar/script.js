8974e765994f1e9219375c50ea648f0bae7c1ff7
mohit2275 <mohit.chauhan2275@gmail.com>
2023-06-22 21:07:59 +0530

    Collapsed Sidebar

---
+++ 46. Collapsed Sidebar/script.js
@@ -0,0 +1,22 @@
+let btn = document.getElementById('btn');
+let cancel = document.getElementById('cancel');
+let nav = document.getElementById('nav');
+let count = 0
+
+btn.addEventListener('click', function () {
+    if (count === 0) {
+        nav.style.width = "30%";
+        document.body.style.marginLeft = "30%"
+        count = 1;
+        console.log('Show');
+    }
+})
+
+cancel.addEventListener('click', function () {
+    if (count === 1) {
+        nav.style.width = "0%"
+        document.body.style.marginLeft = "0%"
+        count = 0;
+        console.log('Hide');
+    }
+})
